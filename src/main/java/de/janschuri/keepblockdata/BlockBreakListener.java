0d372aac724be53139bd8e5fd317cd3653313cf9
Janschuri <124630782+Janschuri@users.noreply.github.com>
2024-07-14 14:28:47 +0200

    removed complete nbt tags

--- src/main/java/de/janschuri/keepblockdata/BlockBreakListener.java
+++ src/main/java/de/janschuri/keepblockdata/BlockBreakListener.java
@@ -8,8 +8,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+
+import java.util.List;
 
 public class BlockBreakListener implements Listener {
 
@@ -21,12 +24,27 @@ public class BlockBreakListener implements Listener {
         if (container.has(KeepBlockData.BLOCK_ITEM_KEY, PersistentDataType.BYTE_ARRAY)) {
 
             byte[] data = container.get(KeepBlockData.BLOCK_ITEM_KEY, PersistentDataType.BYTE_ARRAY);
-            ItemStack oldItem = ItemStackUtils.deserializeItemStack(data);
 
+            container.remove(KeepBlockData.BLOCK_ITEM_KEY);
+
+            ItemStack oldItem = ItemStackUtils.deserializeItemStack(data);
             ItemStack newItem = event.getItems().get(0).getItemStack();
 
             if (oldItem.getType() == newItem.getType()) {
-                newItem.setItemMeta(oldItem.getItemMeta());
+                ItemMeta meta = newItem.getItemMeta();
+
+                String oldName = oldItem.getItemMeta().getDisplayName();
+                List<String> oldLore = oldItem.getItemMeta().getLore();
+
+                if (oldName != null) {
+                    meta.setDisplayName(oldName);
+                }
+
+                if (oldLore != null) {
+                    meta.setLore(oldLore);
+                }
+
+                newItem.setItemMeta(meta);
             }
         }
     }
