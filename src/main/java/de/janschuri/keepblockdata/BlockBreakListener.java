7e462d0ab9dec7d735ecefd7f896ee870aff9710
Janschuri <124630782+Janschuri@users.noreply.github.com>
2025-05-26 17:55:21 +0200

    dupe fix

--- src/main/java/de/janschuri/keepblockdata/BlockBreakListener.java
+++ src/main/java/de/janschuri/keepblockdata/BlockBreakListener.java
@@ -2,16 +2,20 @@ package de.janschuri.keepblockdata;
 
 import com.jeff_media.customblockdata.CustomBlockData;
 import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.io.BukkitObjectInputStream;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class BlockBreakListener implements Listener {
 
@@ -30,7 +34,27 @@ public class BlockBreakListener implements Listener {
             ItemStack newItem = event.getItems().get(0).getItemStack();
 
             if (oldItem.getType() == newItem.getType()) {
-                newItem.setItemMeta(oldItem.getItemMeta());
+                ItemMeta meta = newItem.getItemMeta();
+
+                String oldName = oldItem.getItemMeta().getDisplayName();
+                List<String> oldLore = oldItem.getItemMeta().getLore();
+                Map<Enchantment, Integer> oldEnchants = oldItem.getItemMeta().getEnchants();
+
+                if (oldName != null) {
+                    meta.setDisplayName(oldName);
+                }
+
+                if (oldLore != null) {
+                    meta.setLore(oldLore);
+                }
+
+                if (oldEnchants != null && !oldEnchants.isEmpty()) {
+                    for (Map.Entry<Enchantment, Integer> entry : oldEnchants.entrySet()) {
+                        meta.addEnchant(entry.getKey(), entry.getValue(), true);
+                    }
+                }
+
+                newItem.setItemMeta(meta);
             }
         }
     }
